6815244bed962ebea897c8045f3af8596651779a
headhunter45 <headhunter3@gmail.com>
2011-07-18 18:11:06 -0700

    Initial Import

---
+++ ScoreKeeper/src/com/majinnaibu/bukkit/plugins/scorekeeper/commands/ScoreArchiveCommand.java
@@ -0,0 +1,36 @@
+package com.majinnaibu.bukkit.plugins.scorekeeper.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.majinnaibu.bukkit.plugins.scorekeeper.ScoreKeeperPlugin;
+
+public class ScoreArchiveCommand implements CommandExecutor {
+	private final ScoreKeeperPlugin _plugin;
+	
+	public ScoreArchiveCommand(ScoreKeeperPlugin scoreKeeperPlugin) {
+		_plugin = scoreKeeperPlugin;
+	}
+
+	@Override
+	public boolean onCommand(
+			CommandSender sender,
+			Command command,
+			String label,
+			String[] split
+	) {
+		sender.sendMessage("[" + ChatColor.AQUA + "ScoreKeeper" + ChatColor.WHITE + "] archive command unimplemented ");
+		
+		if(sender instanceof Player){
+			Player player = (Player) sender;
+			int score = _plugin.getPlayerScore(player);
+			player.sendMessage("[" + ChatColor.AQUA + "ScoreKeeper" + ChatColor.WHITE + "] Your score is " + ChatColor.GREEN + score);
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
